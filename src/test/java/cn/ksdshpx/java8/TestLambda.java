22f62a4234d99c2d0365ce338021d98f9289d0eb
Unknown <southeast_px@163.com>
2019-05-08 22:07:36 +0800

    为啥要用Lambda表达式！

--- src/test/java/cn/ksdshpx/java8/TestLambda.java
+++ src/test/java/cn/ksdshpx/java8/TestLambda.java
@@ -16,11 +16,11 @@ public class TestLambda {
      * 使用匿名内部类
      */
     @Test
-    public void test(){
+    public void test() {
         Comparator<Integer> comparator = new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
-                return Integer.compare(o1,o2);
+                return Integer.compare(o1, o2);
             }
         };
         TreeSet<Integer> treeSet = new TreeSet<>(comparator);
@@ -28,23 +28,24 @@ public class TestLambda {
 
     //Lambda表达式
     @Test
-    public void test2(){
-        Comparator<Integer> comparator = (x,y) -> Integer.compare(x,y);
+    public void test2() {
+        Comparator<Integer> comparator = (x, y) -> Integer.compare(x, y);
         TreeSet<Integer> treeSet = new TreeSet<>(comparator);
     }
 
     List<Employee> emps = Arrays.asList(
-            new Employee("zhangSan",30,6666.88),
-            new Employee("liSi",45,7943.99),
-            new Employee("wangWu",18,2333.90),
-            new Employee("zhaoLiu",88,1900.88),
-            new Employee("tianQi",38,3440.85)
+            new Employee("zhangSan", 30, 6666.88),
+            new Employee("liSi", 45, 7943.99),
+            new Employee("wangWu", 18, 2333.90),
+            new Employee("zhaoLiu", 88, 1900.88),
+            new Employee("tianQi", 38, 3440.85)
     );
+
     //需求:获取当前公司中员工年龄大于35的员工信息
-    public List<Employee> filterEmployeeByAge(List<Employee> list){
+    public List<Employee> filterEmployeeByAge(List<Employee> list) {
         List<Employee> emps = new ArrayList<>();
         for (Employee employee : list) {
-            if(employee.getAge() > 35){
+            if (employee.getAge() > 35) {
                 emps.add(employee);
             }
         }
@@ -52,10 +53,10 @@ public class TestLambda {
     }
 
     //需求:获取当前公司中员工工资大于5000的员工信息
-    public List<Employee> filterEmployeeBySalary(List<Employee> list){
+    public List<Employee> filterEmployeeBySalary(List<Employee> list) {
         List<Employee> emps = new ArrayList<>();
         for (Employee employee : list) {
-            if(employee.getSalary() > 5000){
+            if (employee.getSalary() > 5000) {
                 emps.add(employee);
             }
         }
@@ -63,16 +64,16 @@ public class TestLambda {
     }
 
     @Test
-    public void test3(){
+    public void test3() {
         List<Employee> employees = filterEmployeeByAge(emps);
         System.out.println(employees);
     }
 
-    //优化方式一
-    public List<Employee> filterEmployee(List<Employee> list,MyPredicate<Employee> mp){
+    //优化方式一:策略设计模式
+    public List<Employee> filterEmployee(List<Employee> list, MyPredicate<Employee> mp) {
         List<Employee> emps = new ArrayList<>();
         for (Employee employee : list) {
-            if(mp.test(employee)){
+            if (mp.test(employee)) {
                 emps.add(employee);
             }
         }
@@ -80,10 +81,37 @@ public class TestLambda {
     }
 
     @Test
-    public void test4(){
-        List<Employee> employees = filterEmployee(emps,new FilterEmployeeByAge());
+    public void test4() {
+        List<Employee> employees = filterEmployee(emps, new FilterEmployeeByAge());
         System.out.println(employees);
-        employees = filterEmployee(emps,new FilterEmployeeBySalary());
+        employees = filterEmployee(emps, new FilterEmployeeBySalary());
         System.out.println(employees);
+    }
+
+    //优化方式二:匿名内部类
+    @Test
+    public void test5() {
+        List<Employee> employees = filterEmployee(emps, new MyPredicate<Employee>() {
+            @Override
+            public boolean test(Employee employee) {
+                return employee.getSalary() < 5000;
+            }
+        });
+        System.out.println(employees);
+    }
+
+    //优化方式三:Lambda表达式
+    @Test
+    public void test6() {
+        List<Employee> employees = filterEmployee(emps, (e) -> e.getSalary() < 5000);
+        employees.forEach(System.out::println);
+    }
+
+    //优化方式四:Stream API
+    @Test
+    public void test7() {
+        emps.stream()
+                .filter((e) -> e.getSalary() > 5000)
+                .forEach(System.out::println);
     }
 }
