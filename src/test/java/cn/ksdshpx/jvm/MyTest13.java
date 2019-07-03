915d4c59281f39af75c4a1610e36a67e1ae23170
ksdshpx <ksdshpx2018@aliyun.com>
2019-07-03 11:09:38 +0800

    Java字节码文件结构分析!

---
+++ src/test/java/cn/ksdshpx/jvm/MyTest13.java
@@ -0,0 +1,20 @@
+package cn.ksdshpx.jvm;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.xing
+ * Date: 2019/7/3
+ * Time: 10:24
+ * Description:Java字节码文件结构分析
+ */
+public class MyTest13 {
+    private int a = 1;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
