2c4e264b1b637d746a352da3cef2196f6b6024d5
ksdshpx <ksdshpx2018@aliyun.com>
2019-09-06 11:01:49 +0800

    lombok之@Data!

---
+++ src/test/java/cn/ksdshpx/lombok/Teacher.java
@@ -0,0 +1,18 @@
+package cn.ksdshpx.lombok;
+
+import lombok.Data;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.xing
+ * Date: 2019/9/6
+ * Time: 11:00
+ * Description:Teacher实体类
+ */
+@Data
+public class Teacher {
+    private String name;
+    private Integer age;
+    private String email;
+    private Boolean flag;
+}
